c1bbbd4a2105a3ad05c08748ecc15eeb0c9d3dac
EloyTejero <tejero.eloyet36@gmail.com>
2023-09-04 00:12:15 -0300

    ADD switch de player y control de escritura sobre casillas ya escritas

--- script.js
+++ script.js
@@ -5,14 +5,35 @@ const casillas = [
     document.getElementById("casilla-7"),document.getElementById("casilla-8"),
     document.getElementById("casilla-9")];
 
+let player = "x";
+
+function tooglePlayer(){
+    if(player=="x"){
+        player = "o";
+    }
+    else{
+        player = "x";
+    }
+}
+
+function isCellEmpty(cell){
+    if(cell.innerText != ""){
+        return true;
+    }
+    return false;
+}
+
 for(let i = 0; i<casillas.length; i++){
     let casilla = casillas[i];
     casilla.addEventListener("click", casillaClickeada);
 }
 
 function casillaClickeada(){
-    this.innerHTML = "hola";
-    console.log("chau")
+    if(!isCellEmpty(this)){
+        this.innerHTML = player;
+        tooglePlayer();
+    }
+    console.log(player);
 } //añadir control para cuando ya hay un valor en la casilla y que cuando se gane ya no se pueda tocar
 //osea aunque quede una casilla vacia no se pueda escribir en esta
 
